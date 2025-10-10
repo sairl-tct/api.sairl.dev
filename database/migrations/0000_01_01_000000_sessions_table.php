f19f12ed87f9afa5d715ef8965539df42de29900
unknown <hengkakada899@gmail.com>
2025-10-10 23:47:14 +0700

    chore: move session to own migration

--- database/migrations/0000_01_01_000000_sessions_table.php
+++ database/migrations/0000_01_01_000000_sessions_table.php
@@ -1,10 +1,13 @@
 <?php
 
+declare(strict_types=1);
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
     public function up(): void
     {
         Schema::create('sessions', function (Blueprint $table): void {
@@ -16,5 +19,4 @@ return new class extends Migration {
             $table->integer('last_activity')->index();
         });
     }
-
 };
