59c48dec329ca8a876f4e62834a4e13259a13b08
unknown <hengkakada899@gmail.com>
2025-10-14 16:14:07 +0700

    chore: remove sactumn

--- app/Actions/Queries/Statuses/GetStatus.php
+++ app/Actions/Queries/Statuses/GetStatus.php
@@ -6,7 +6,8 @@ namespace App\Actions\Queries\Statuses;
 
 use App\Models\Status;
 
-final class GetStatus {
+final class GetStatus
+{
     public function handle(int $id): ?Status
     {
         return Status::query()->where('id', $id)->first();
