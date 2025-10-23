139b68c7b4cb55fa0b34ef17a7297d39c8e16a96
houmenghor <menghorhou@gmail.com>
2025-10-23 16:15:42 +0700

    fix:lint

--- database/migrations/2025_10_16_083012_create_roles_table.php
+++ database/migrations/2025_10_16_083012_create_roles_table.php
@@ -8,24 +8,13 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('roles', function (Blueprint $table): void {
-            $table->unsignedTinyInteger('id', true)->primary();
+            $table->smallIncrements('id')->primary();
             $table->string('name', 50)->unique();
             $table->string('description', 255)->nullable();
             $table->timestamps();
         });
-    }
-
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('roles');
     }
 };
