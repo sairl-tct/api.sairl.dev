a9cab87e85131a2e8d17b77b5f8655349d78f8e6
Hou Menghor <160103302+houmenghor@users.noreply.github.com>
2025-10-27 16:28:15 +0700

    Feat/team tag (#29)

---
+++ app/Actions/TeamTags/UpdateTeamTag.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Actions\TeamTags;
+
+use App\Models\TeamTag;
+
+final class UpdateTeamTag
+{
+    public function handle(int $id, array $data): array
+    {
+        $TeamTag = TeamTag::query()->find($id);
+
+        if (is_null($TeamTag)) {
+            return [
+                'status' => 'error',
+                'message' => 'Team tag not found',
+                'code' => 404,
+            ];
+        }
+
+        $duplicate = TeamTag::query()
+            ->where('name', $data['name'])
+            ->whereNot('id', $id)
+            ->exists();
+
+        if ($duplicate) {
+            return [
+                'status' => 'error',
+                'message' => 'The name is already taken',
+                'code' => 422,
+            ];
+        }
+        $TeamTag->update($data);
+
+        return [
+            'status' => 'success',
+            'message' => 'Team tag successfully updated',
+            'code' => 200,
+        ];
+    }
+}
