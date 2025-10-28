b16aadcfc38597c881940b71d7837482678924ff
houmenghor <menghorhou@gmail.com>
2025-10-28 16:04:06 +0700

    fix(lint)

---
+++ app/Actions/TeamCategories/CreateTeamCategory.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Actions\TeamCategories;
+
+use App\Models\TeamCategory;
+
+
+final class CreateTeamCategory {
+    /**
+     *
+     * @param array<string, mixed> $data
+     */
+    public function handle(array $data): TeamCategory
+    {
+        return TeamCategory::query()->create($data);
+    }
+}
---
+++ app/Actions/TeamCategories/UpdateTeamCategory.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Actions\TeamCategories;
+
+use App\Models\TeamCategory;
+
+final class UpdateTeamCategory {
+    /**
+     *
+     * @param array<string, mixed> $data
+     * @return array<string, mixed>
+     */
+    public function handle(int $id, array $data): array
+    {
+        $TeamCategory = TeamCategory::query()->find($id);
+        if (is_null($TeamCategory)) {
+            return [
+                'status' => 'error',
+                'message' => 'Team category not found',
+                'code' => 404
+            ];
+        }
+
+        $duplicate = TeamCategory::query()
+            ->where('name', $data['name'])
+            ->whereNot('id', $id)
+            ->exists();
+        if ($duplicate) {
+            return [
+                'status' => 'error',
+                'message' => 'The name is already taken',
+                'code' => 422
+            ];
+        }
+        $TeamCategory->update($data);
+        return [
+            'status' => 'success',
+            'message' => 'Team category updated successfully',
+            'code' => 200
+        ];
+    }
+}
